c5dc58cbf0873002e2707dab75d2a4868df05ede
devetry-brandon <brandon.pfeiffer@devetry.com>
2022-03-11 11:00:50 -0500

    Add typescript

---
+++ packages/sample-package/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sample() {
+    return "sample package";
+}
+exports.default = sample;
+//# sourceMappingURL=index.js.map
---
+++ packages/sample-package/src/index.ts
@@ -0,0 +1,3 @@
+export default function sample(): string {
+    return "sample package";
+}
