27c7d738929b58f128e57cfff9eef3fcc9b7a8db
devetry-brandon <brandon.pfeiffer@devetry.com>
2022-03-14 09:52:57 -0400

    Get rid of generic serivce and make adobe-sign-controller more complex.

--- lambdas/adobe-sign-controller/src/handler.ts
+++ lambdas/adobe-sign-controller/src/handler.ts
@@ -1,6 +1,11 @@
-export async function getAgreement(event, context) {
+import { AdobeSignService } from "adobe-sign";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const getAgreement = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let service = new AdobeSignService();
+    let agreement = service.getAgreement(event.queryStringParameters.id);
     return {
         statusCode: 200,
-        body: `Here is an agreement`
+        body: JSON.stringify(agreement)
     };
 }
--- lambdas/service1/src/handler.ts
+++
@@ -1,6 +0,0 @@
-export async function main(event, context) {
-    return {
-        statusCode: 200,
-        body: `Hello World! This is a typescript handler.`,
-    };
-}
