cc2dd7b046b283d5d8eac0cd7b95b0d8ac186987
devetry-brandon <brandon.pfeiffer@devetry.com>
2022-03-16 09:14:44 -0400

    Move jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/src/**'
+  ],
+  coveragePathIgnorePatterns: [
+    "enums",
+    "providers",
+    "testing"
+  ]
+};
