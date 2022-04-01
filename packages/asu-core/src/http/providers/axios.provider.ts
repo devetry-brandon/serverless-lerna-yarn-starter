33a6c46dfd8895a4aaa665631aca2fb480ca957b
noahleapai <noah.leapai@devetry.com>
2022-04-01 17:26:55 -0500

    Remove axios error experimentation from provider

--- packages/asu-core/src/http/providers/axios.provider.ts
+++ packages/asu-core/src/http/providers/axios.provider.ts
@@ -1,11 +1,7 @@
-import {Axios, AxiosError, AxiosRequestConfig, default as axios} from "axios";
+import {Axios, AxiosRequestConfig, default as axios} from "axios";
 
 export class AxiosProvider {
   public resolve(config: AxiosRequestConfig): Axios {
     return axios.create(config);
-  }
-
-  public isAxiosError(payload: any): payload is AxiosError {
-    return axios.isAxiosError(payload);
   }
 }
